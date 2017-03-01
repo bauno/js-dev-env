dec5a7047d4dba1f5f0436db52f9a7eec383f3e5
Alessandro Bonometti <a.bonometti@gmail.com>
2017-03-01 17:34:32 +0100

    trying to recover

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -8,6 +8,17 @@ function get(url){
   return fetch(url).then(onSuccess, onError)
 }
 
+export function deleteUser(id){
+  return del(`users/${id}`)
+}
+
+function del( url) {
+  const request = new Request(baseUrl + url,  {
+    method: 'DELETE'
+  })
+  return fetch(request).then(onSuccess, onError)
+}
+
 function onSuccess(response) {
   return response.json()
 }
