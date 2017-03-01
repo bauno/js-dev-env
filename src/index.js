29f3a57b26c967eaa12fe30a9dd72f9a6b831659
Alessandro Bonometti <a.bonometti@gmail.com>
2017-03-01 17:51:51 +0100

    Incredibly, it's working :-)

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ getUsers().then(result => {
 
   global.document.getElementById('users').innerHTML = usersBody
 
-  const deleteLinks = global.document.getElementByClassName("deleteUser")
+  const deleteLinks = global.document.getElementsByClassName("deleteUser")
   Array.from(deleteLinks, link => {
-    link.onClick = function(event) {
+    link.onclick = function(event) {
       const element = event.target
       event.preventDefault()
       deleteUser(element.attributes["data-id"].value)
